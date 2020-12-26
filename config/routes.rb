620e6b5f98ab01f1e3369d8322804ea835f200f7
vagrant <vagrant@localhost.localdomain>
2020-12-26 18:41:00 +0000

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_for :users
   root 'maps#index'
-  resources :maps, except: [:index, :new]
+  resources :maps, except: [:index, :new] do
+  	resource :favorites, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
