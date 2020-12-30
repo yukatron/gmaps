ce7888743c04e40f1078a600e5e67cf33d5be348
vagrant <vagrant@localhost.localdomain>
2020-12-30 16:23:41 +0000

    [edit]devise views

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,26 +1,7 @@
-function initMap() {
-
-    var test ={lat: <%= @map.latitude %>, lng: <%= @map.longitude %>};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: test
-        <% binding.pry %>
+var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
     });
-    var transitLayer = new google.maps.TransitLayer();
-    transitLayer.setMap(map);
-
-    var contentString = '住所：<%= @map.address %>';
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-
-    var marker = new google.maps.Marker({
-        position:test,
-        map: map,
-        title: contentString
-    });
-
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
-}
+  }
