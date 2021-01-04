8ee8848c21b35e2b3cc264f93f20746a4f8215b5
vagrant <vagrant@localhost.localdomain>
2021-01-04 16:25:42 +0000

    [fix]google map

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'maps#index'
-  resources :maps, except: [:index, :new] do
+  resources :maps, except: [:new] do
   	resource :favorites, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
