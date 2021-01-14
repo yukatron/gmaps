33c087b1a6c4f38ed11efc49e16ff48931339d0c
vagrant <vagrant@localhost.localdomain>
2021-01-14 16:24:02 +0000

    [fixing]geocoding

--- app/models/map.rb
+++ app/models/map.rb
@@ -2,8 +2,6 @@ class Map < ApplicationRecord
 
 	validates :address, presence: true
 	validates :name, presence: true
-	validates :longitude, presence: true
-	validates :latitude, presence: true
 
 	geocoded_by :address
     after_validation :geocode, if: :address_changed?
