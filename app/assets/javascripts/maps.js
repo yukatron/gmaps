3ad38e7deba2d87a38b0d93c01294842fe4e3cb7
vagrant <vagrant@localhost.localdomain>
2021-08-18 16:21:32 +0000

    issues with geocoder

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -12,3 +12,21 @@ function initMap(){
         map: map
     });
 }
+
+let geocoder
+
+function codeAddress(){
+    let inputAddress = document.getElementById('address').value;
+
+    geocoder.geocode( { 'address': inputAddress}, function(results, status) {
+        if (status == 'OK') {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('該当する結果がありませんでした:' + status);
+        }
+    });
+}
